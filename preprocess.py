317f8dac0c878956fa261264184c091c50b2f0bc
Daniel Jo <67842986+danielskjo@users.noreply.github.com>
2023-03-16 09:23:50 -0700

    Added new dataset

---
+++ preprocess.py
@@ -0,0 +1,192 @@
+from collections import defaultdict
+import itertools
+import os
+import pickle
+import random
+
+import networkx as nx
+from networkx.algorithms import bipartite as bi
+import numpy as np
+from scipy.io import loadmat
+
+
+def preprocess(path):
+    uSet_u2u = set()
+    uSet_u2b = set()
+    bSet_u2b = set()
+
+    social_adj_lists = defaultdict(set)
+    history_u_lists = defaultdict(list)
+    history_v_lists = defaultdict(list)
+
+    history_ur_lists = defaultdict(list)
+    history_vr_lists = defaultdict(list)
+
+    G = nx.Graph()
+    G.name = 'ciao'
+
+    ratings_f = loadmat(path + 'epinions/rating.mat')['rating']
+    trust_f = loadmat(path + 'epinions/trustnetwork.mat')['trustnetwork']
+
+    users = []
+
+    for s in ratings_f:
+        uid = s[0]
+        users.append(uid)
+
+    offset = len(set(users))
+
+    for s in ratings_f:
+        uid = s[0]
+        iid = s[1] + offset
+        rating = s[3]
+        uSet_u2b.add(uid)
+        bSet_u2b.add(iid)
+        G.add_edge(uid, iid, type='u2b', rating=rating)
+
+    for s in trust_f:
+        uid = s[0]
+        fid = s[1]
+        uSet_u2u.add(uid)
+        uSet_u2u.add(fid)
+        G.add_edge(uid, fid, type='u2u')
+
+    print(G)
+    print("uSet of u2u, size: " + str(len(uSet_u2u)))
+    print("uSet of u2b, size: " + str(len(uSet_u2b)))
+    print("bSet of u2b, size: " + str(len(bSet_u2b)))
+
+    G = nx.convert_node_labels_to_integers(
+        G, first_label=0, ordering='default', label_attribute="name")
+
+    node_names = nx.get_node_attributes(G, 'name')
+    inv_map = {v: k for k, v in node_names.items()}
+
+    uSet_u2u = set([inv_map.get(name) for name in uSet_u2u])
+    uSet_u2b = set([inv_map.get(name) for name in uSet_u2b])
+    bSet_u2b = set([inv_map.get(name) for name in bSet_u2b])
+
+    edge_list_uv = []
+    edge_list_vu = []
+
+    for node in G:
+        for nbr in G[node]:
+            if G[node][nbr]['type'] == 'u2u':
+                social_adj_lists[node].add(nbr)
+
+            if G[node][nbr]['type'] == 'u2b':
+                r = G[node][nbr]['rating'] - 1
+
+                if node in uSet_u2b and nbr in bSet_u2b:
+                    history_u_lists[node].append(nbr)
+                    history_v_lists[nbr].append(node)
+                    history_ur_lists[node].append(r)
+                    history_vr_lists[nbr].append(r)
+                    edge_list_uv.append((node, nbr, r))
+                    edge_list_vu.append((nbr, node, r))
+
+                if nbr in uSet_u2b and node in bSet_u2b:
+                    history_u_lists[nbr].append(node)
+                    history_v_lists[node].append(nbr)
+                    history_ur_lists[nbr].append(r)
+                    history_vr_lists[node].append(r)
+                    edge_list_uv.append((nbr, node, r))
+                    edge_list_vu.append((node, nbr, r))
+
+    data = []
+    for (u, v) in G.edges():
+        if G[u][v]['type'] == 'u2b':
+            r = G[u][v]['rating'] - 1
+
+            if u in uSet_u2b:
+                data.append((u, v, r))
+            else:
+                data.append((v, u, r))
+
+    random.shuffle(data)
+
+    size = len(data)
+    train_data = data[:int(0.8 * size)]
+    test_data = data[int(0.8 * size):]
+
+    with open(path + 'dataset.pkl', 'wb') as f:
+        pickle.dump(train_data, f, pickle.HIGHEST_PROTOCOL)
+        pickle.dump(test_data, f, pickle.HIGHEST_PROTOCOL)
+
+    train_u, train_v, train_r, test_u, test_v, test_r = [], [], [], [], [], []
+
+    for u, v, r in train_data:
+        train_u.append(u)
+        train_v.append(v)
+        train_r.append(r)
+
+    for u, v, r in test_data:
+        test_u.append(u)
+        test_v.append(v)
+        test_r.append(r)
+
+    ratings_list = [0, 1, 2, 3, 4]
+
+    _social_adj_lists = defaultdict(set)
+    _history_u_lists = defaultdict(list)
+    _history_v_lists = defaultdict(list)
+
+    _history_ur_lists = defaultdict(list)
+    _history_vr_lists = defaultdict(list)
+    _train_u, _train_v, _train_r, _test_u, _test_v, _test_r = [], [], [], [], [], []
+
+    social_id_dic = {v: k for k, v in dict(
+        enumerate(social_adj_lists.keys())).items()}
+    user_id_dic = {v: k for k, v in dict(
+        enumerate(history_u_lists.keys())).items()}
+    item_id_dic = {v: k for k, v in dict(
+        enumerate(history_v_lists.keys())).items()}
+
+    for u in history_u_lists:
+        _history_u_lists[user_id_dic[u]] = [item_id_dic[v]
+                                            for v in history_u_lists[u]]
+
+    for v in history_v_lists:
+        _history_v_lists[item_id_dic[v]] = [user_id_dic[u]
+                                            for u in history_v_lists[v]]
+
+    for u in history_ur_lists:
+        _history_ur_lists[user_id_dic[u]] = history_ur_lists[u]
+
+    for v in history_vr_lists:
+        _history_vr_lists[item_id_dic[v]] = history_vr_lists[v]
+
+    for u in social_adj_lists:
+        _social_adj_lists[social_id_dic[u]] = [social_id_dic[us]
+                                               for us in social_adj_lists[u]]
+
+    for u, v, r in train_data:
+        if u in user_id_dic.keys() and v in item_id_dic.keys():
+            _train_u.append(user_id_dic[u])
+            _train_v.append(item_id_dic[v])
+            _train_r.append(r)
+
+    for u, v, r in test_data:
+        if u in user_id_dic.keys() and v in item_id_dic.keys():
+            _test_u.append(user_id_dic[u])
+            _test_v.append(item_id_dic[v])
+            _test_r.append(r)
+
+    with open(path + 'list.pkl', 'wb') as f:
+        pickle.dump(_history_u_lists, f, pickle.HIGHEST_PROTOCOL)
+        pickle.dump(_history_ur_lists, f, pickle.HIGHEST_PROTOCOL)
+        pickle.dump(_history_v_lists, f, pickle.HIGHEST_PROTOCOL)
+        pickle.dump(_history_vr_lists, f, pickle.HIGHEST_PROTOCOL)
+        pickle.dump(_train_u, f, pickle.HIGHEST_PROTOCOL)
+        pickle.dump(_train_v, f, pickle.HIGHEST_PROTOCOL)
+        pickle.dump(_train_r, f, pickle.HIGHEST_PROTOCOL)
+        pickle.dump(_test_u, f, pickle.HIGHEST_PROTOCOL)
+        pickle.dump(_test_v, f, pickle.HIGHEST_PROTOCOL)
+        pickle.dump(_test_r, f, pickle.HIGHEST_PROTOCOL)
+        pickle.dump(_social_adj_lists, f, pickle.HIGHEST_PROTOCOL)
+        pickle.dump(ratings_list, f, pickle.HIGHEST_PROTOCOL)
+
+
+if __name__ == "__main__":
+    path = 'data/'
+    preprocess(path)
--- run_GraphRec_example.py
+++ run_GraphRec_example.py
@@ -90,7 +90,8 @@ def train(model, device, train_loader, optimizer, epoch, best_rmse, best_mae):
     for i, data in enumerate(train_loader, 0):
         batch_nodes_u, batch_nodes_v, labels_list = data
         optimizer.zero_grad()
-        loss = model.loss(batch_nodes_u.to(device), batch_nodes_v.to(device), labels_list.to(device))
+        loss = model.loss(batch_nodes_u.to(device),
+                          batch_nodes_v.to(device), labels_list.to(device))
         loss.backward(retain_graph=True)
         optimizer.step()
         running_loss += loss.item()
@@ -101,16 +102,26 @@ def train(model, device, train_loader, optimizer, epoch, best_rmse, best_mae):
     return 0
 
 
-def test(model, device, test_loader):
+def test(model, device, test_loader, epoch):
     model.eval()
     tmp_pred = []
     target = []
     with torch.no_grad():
+        f = open(f"epoch/{epoch}.txt", "a")
+
         for test_u, test_v, tmp_target in test_loader:
-            test_u, test_v, tmp_target = test_u.to(device), test_v.to(device), tmp_target.to(device)
+            test_u, test_v, tmp_target = test_u.to(
+                device), test_v.to(device), tmp_target.to(device)
             val_output = model.forward(test_u, test_v)
             tmp_pred.append(list(val_output.data.cpu().numpy()))
             target.append(list(tmp_target.data.cpu().numpy()))
+
+            for i in range(len(test_u)):
+                f.write(
+                    f"{test_u[i]} {test_v[i]} {val_output[i]} {tmp_target[i]}\n")
+
+        f.write("\n")
+
     tmp_pred = np.array(sum(tmp_pred, []))
     target = np.array(sum(target, []))
     expected_rmse = sqrt(mean_squared_error(tmp_pred, target))
@@ -120,27 +131,54 @@ def test(model, device, test_loader):
 
 def main():
     # Training settings
-    parser = argparse.ArgumentParser(description='Social Recommendation: GraphRec model')
-    parser.add_argument('--batch_size', type=int, default=128, metavar='N', help='input batch size for training')
-    parser.add_argument('--embed_dim', type=int, default=64, metavar='N', help='embedding size')
-    parser.add_argument('--lr', type=float, default=0.001, metavar='LR', help='learning rate')
-    parser.add_argument('--test_batch_size', type=int, default=1000, metavar='N', help='input batch size for testing')
-    parser.add_argument('--epochs', type=int, default=100, metavar='N', help='number of epochs to train')
+    parser = argparse.ArgumentParser(
+        description='Social Recommendation: GraphRec model')
+    parser.add_argument('--batch_size', type=int, default=128,
+                        metavar='N', help='input batch size for training')
+    parser.add_argument('--embed_dim', type=int, default=64,
+                        metavar='N', help='embedding size')
+    parser.add_argument('--lr', type=float, default=0.001,
+                        metavar='LR', help='learning rate')
+    parser.add_argument('--test_batch_size', type=int, default=1000,
+                        metavar='N', help='input batch size for testing')
+    parser.add_argument('--epochs', type=int, default=100,
+                        metavar='N', help='number of epochs to train')
     args = parser.parse_args()
 
-    os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+    os.environ['CUDA_VISIBLE_DEVICES'] = '1'
     use_cuda = False
     if torch.cuda.is_available():
         use_cuda = True
     device = torch.device("cuda" if use_cuda else "cpu")
 
     embed_dim = args.embed_dim
-    dir_data = './data/toy_dataset'
 
-    path_data = dir_data + ".pickle"
-    data_file = open(path_data, 'rb')
-    history_u_lists, history_ur_lists, history_v_lists, history_vr_lists, train_u, train_v, train_r, test_u, test_v, test_r, social_adj_lists, ratings_list = pickle.load(
-        data_file)
+    with open('data/list.pkl', 'rb') as f:
+        """
+        history_u_lists, history_ur_lists:  user's purchased history (item set in training set), and his/her rating score (dict)
+        history_v_lists, history_vr_lists:  user set (in training set) who have interacted with the item, and rating score (dict)
+        train_u, train_v, train_r: training_set (user, item, rating)
+        test_u, test_v, test_r: testing set (user, item, rating)
+        """
+        # Users' history of purchased items
+        history_u_lists = pickle.load(f)
+
+        # Users' ratings on their purchased items
+        history_ur_lists = pickle.load(f)
+
+        # Items' history of users who purchased it
+        history_v_lists = pickle.load(f)
+
+        # Item's ratings by the users who purchased it
+        history_vr_lists = pickle.load(f)
+        train_u = pickle.load(f)
+        train_v = pickle.load(f)
+        train_r = pickle.load(f)
+        test_u = pickle.load(f)
+        test_v = pickle.load(f)
+        test_r = pickle.load(f)
+        social_adj_lists = pickle.load(f)
+        ratings_list = pickle.load(f)
     """
     ## toy dataset 
     history_u_lists, history_ur_lists:  user's purchased history (item set in training set), and his/her rating score (dict)
@@ -159,11 +197,16 @@ def main():
                                               torch.FloatTensor(train_r))
     testset = torch.utils.data.TensorDataset(torch.LongTensor(test_u), torch.LongTensor(test_v),
                                              torch.FloatTensor(test_r))
-    train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(testset, batch_size=args.test_batch_size, shuffle=True)
+    train_loader = torch.utils.data.DataLoader(
+        trainset, batch_size=args.batch_size, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(
+        testset, batch_size=args.test_batch_size, shuffle=True)
     num_users = history_u_lists.__len__()
     num_items = history_v_lists.__len__()
     num_ratings = ratings_list.__len__()
+
+    print("number of users, items, ratings: ",
+          num_users, num_items, num_ratings)
 
     u2e = nn.Embedding(num_users, embed_dim).to(device)
     v2e = nn.Embedding(num_items, embed_dim).to(device)
@@ -171,20 +214,26 @@ def main():
 
     # user feature
     # features: item * rating
-    agg_u_history = UV_Aggregator(v2e, r2e, u2e, embed_dim, cuda=device, uv=True)
-    enc_u_history = UV_Encoder(u2e, embed_dim, history_u_lists, history_ur_lists, agg_u_history, cuda=device, uv=True)
+    agg_u_history = UV_Aggregator(
+        v2e, r2e, u2e, embed_dim, cuda=device, uv=True)
+    enc_u_history = UV_Encoder(u2e, embed_dim, history_u_lists,
+                               history_ur_lists, agg_u_history, cuda=device, uv=True)
     # neighobrs
-    agg_u_social = Social_Aggregator(lambda nodes: enc_u_history(nodes).t(), u2e, embed_dim, cuda=device)
+    agg_u_social = Social_Aggregator(lambda nodes: enc_u_history(
+        nodes).t(), u2e, embed_dim, cuda=device)
     enc_u = Social_Encoder(lambda nodes: enc_u_history(nodes).t(), embed_dim, social_adj_lists, agg_u_social,
                            base_model=enc_u_history, cuda=device)
 
     # item feature: user * rating
-    agg_v_history = UV_Aggregator(v2e, r2e, u2e, embed_dim, cuda=device, uv=False)
-    enc_v_history = UV_Encoder(v2e, embed_dim, history_v_lists, history_vr_lists, agg_v_history, cuda=device, uv=False)
+    agg_v_history = UV_Aggregator(
+        v2e, r2e, u2e, embed_dim, cuda=device, uv=False)
+    enc_v_history = UV_Encoder(v2e, embed_dim, history_v_lists,
+                               history_vr_lists, agg_v_history, cuda=device, uv=False)
 
     # model
     graphrec = GraphRec(enc_u, enc_v_history, r2e).to(device)
-    optimizer = torch.optim.RMSprop(graphrec.parameters(), lr=args.lr, alpha=0.9)
+    optimizer = torch.optim.RMSprop(
+        graphrec.parameters(), lr=args.lr, alpha=0.9)
 
     best_rmse = 9999.0
     best_mae = 9999.0
@@ -192,7 +241,8 @@ def main():
 
     for epoch in range(1, args.epochs + 1):
 
-        train(graphrec, device, train_loader, optimizer, epoch, best_rmse, best_mae)
+        train(graphrec, device, train_loader,
+              optimizer, epoch, best_rmse, best_mae)
         expected_rmse, mae = test(graphrec, device, test_loader)
         # please add the validation set to tune the hyper-parameters based on your datasets.
 
